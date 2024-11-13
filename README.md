a877e0bc9cf3319ea5ebf3eefd24d31c9f025d71
siwuxie <cwl990110@163.com>
2024-11-14 01:15:34 +0800

    feat(learn_tokio): 添加 mini-redis 依赖并实现异步 Redis 操作
    - 在 Cargo.toml 中添加 mini-redis 依赖
    - 在 src/main.rs 中实现了一个简单的 Redis 客户端示例
    - 新增 README.md 文件，记录学习进度

---
+++ README.md
@@ -0,0 +1,6 @@
+# rs_learn_tokio
+
+
+<!-- 
+2024年11月14日01:14:40 学到这了 https://course.rs/advance-practice/getting-startted.html
+ -->
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+
+
+#[tokio::main]
+async fn main() -> Result<(),mini_redis::Error> {
+    // 建立与mini-redis服务器的连接
+    let mut client = mini_redis::client::connect("127.0.0.1:6379").await?;
+
+    // 设置 key: "hello" 和 值: "world"
+    client.set("hello", "1world".into()).await?;
+
+    // 获取"key=hello"的值
+    let result = client.get("hello").await?;
+
+    println!("从服务器端获取到结果={:?}", result);
+
+    Ok(())
 }
